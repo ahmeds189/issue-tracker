6bdbea5815b33ef2d4833e30d7ebb4ed00472e67
ahmed <primaryahmedsaid@gmail.com>
2023-10-16 01:38:21 +0300

    building new issue form with markdown editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,11 +1,17 @@
+'use client'
 import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
 import Wrapper from '@/components/wrapper'
+import SimpleMDE from 'react-simplemde-editor'
+import 'easymde/dist/easymde.min.css'
 
 const NewIssue = () => {
 	return (
 		<Wrapper>
-			<form data-color-mode='light'>
+			<form data-color-mode='light' className='space-y-5 max-w-xl mx-auto'>
 				<Input placeholder='Title' />
+				<SimpleMDE placeholder='Description' options={{ status: false }} />
+				<Button type='submit'>Submit</Button>
 			</form>
 		</Wrapper>
 	)
