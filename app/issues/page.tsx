b4f7527875ff2501d48f0e1dd1a7d24c8e112edc
ahmed <primaryahmedsaid@gmail.com>
2023-10-17 05:33:26 +0300

    test data fetch and html parse description

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,24 +1,28 @@
+import IssuesTable from '@/components/IssuesTable'
 import { buttonVariants } from '@/components/ui/button'
 import Wrapper from '@/components/wrapper'
 import { cn } from '@/lib/utils'
+import prisma from '@/prisma/client'
 import Link from 'next/link'
+import parse from 'html-react-parser'
+import IssueBadge from '@/components/IssueBadge'
 
-const Issue = () => {
+const Issue = async () => {
+	const issues = await prisma.issue.findMany()
+
 	return (
 		<Wrapper>
-			<header className='flex justify-between'>
+			<header className='flex justify-between mb-12'>
 				<h1 className='font-bold text-3xl'>Issues</h1>
-				<Link
-					href='/issues/new'
-					className={cn(
-						buttonVariants(),
-						'bg-violet-600 text-lg hover:bg-violet-500'
-					)}
-				>
+				<Link href='/issues/new' className={cn(buttonVariants(), 'text-lg')}>
 					New Issue
 				</Link>
 			</header>
-			<p>all issues here</p>
+			<IssuesTable />
+			<IssueBadge status={issues[2].status} />
+			<IssueBadge status={issues[3].status} />
+			<IssueBadge status={issues[4].status} />
+			{parse(issues[2].description)}
 		</Wrapper>
 	)
 }
