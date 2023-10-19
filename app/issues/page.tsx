6b82511ed9ca4ab57118030528974b78d68ca720
ahmed <primaryahmedsaid@gmail.com>
2023-10-19 22:40:34 +0300

    use react Suspense to render skeleton

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,28 +1,26 @@
-import IssuesTable from '@/components/IssuesTable'
+import IssuesTable from '@/app/issues/IssuesTable'
+import TableSkeleton from '@/app/issues/TableSkeleton'
 import { buttonVariants } from '@/components/ui/button'
 import Wrapper from '@/components/wrapper'
 import { cn } from '@/lib/utils'
-import prisma from '@/prisma/client'
 import Link from 'next/link'
-import parse from 'html-react-parser'
-import IssueBadge from '@/components/IssueBadge'
+import { Suspense } from 'react'
 
-const Issue = async () => {
-	const issues = await prisma.issue.findMany()
-
+const Issue = () => {
 	return (
 		<Wrapper>
 			<header className='flex justify-between mb-12'>
 				<h1 className='font-bold text-3xl'>Issues</h1>
-				<Link href='/issues/new' className={cn(buttonVariants(), 'text-lg')}>
+				<Link
+					href='/issues/new'
+					className={cn(buttonVariants(), 'font-semibold')}
+				>
 					New Issue
 				</Link>
 			</header>
-			<IssuesTable />
-			<IssueBadge status={issues[2].status} />
-			<IssueBadge status={issues[3].status} />
-			<IssueBadge status={issues[4].status} />
-			{parse(issues[2].description)}
+			<Suspense fallback={<TableSkeleton />}>
+				<IssuesTable />
+			</Suspense>
 		</Wrapper>
 	)
 }
